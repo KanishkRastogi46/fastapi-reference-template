fa00e2256ceccbb01644174ec0f711399bfc544f
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-04-09 19:59:42 +0530

    adding templates and static files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
 from dotenv import load_dotenv
-import os
 from db import Base, engine
 
 load_dotenv()
@@ -14,6 +14,11 @@ from dependencies import get_db
 
 app = FastAPI()
 
+# adding static files and templates
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# adding api routers
 from routers import users
 app.include_router(users.router, prefix="/users", tags=["users"])
 
@@ -26,6 +31,10 @@ app.add_middleware(
 )
 
 
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", context={'request':request})
+
 @app.get("/hello/{name}")
 def index(request: Request, name: str, q: str = None):
     print(request.headers)
@@ -37,5 +46,5 @@ def index(request: Request, name: str, q: str = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=app)
+    uvicorn.run(app=app, host='0.0.0.0', port=8000, reload=True)
     get_db()
